00a34a47903c8a41736c40c22d75c4e23bda8202
Karnap <karnapp2@illinois.edu>
2020-02-14 17:16:34 -0600

    test

--- src/publisher.cpp
+++ src/publisher.cpp
@@ -2,5 +2,19 @@
 #include "std_msgs/String.h"
 #include <sstream>
 
-int main(int argc, char)
+int main(int argc, char ** argv) {
+
+    ros::init(argc, argv,'publisher');
+
+    ros::NodeHandle nh;
+
+    ros::Publisher pos_publish = nh.advertise<geometry_msgs::PoseStamped>
+        ("mavros/setpoint_position/local", 10);
+
+    geometry_msgs::PoseStamped pose;
+    pose.position.x = 0;
+    pose.position.y = 0;
+    pose.position.z = 0;
+
+}
 
