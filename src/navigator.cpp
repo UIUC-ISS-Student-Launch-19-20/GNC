a6f284b6c90ece02b44ad53ff71348bac0254ae4
AyberkY <ayberkyaraneri@gmail.com>
2020-02-15 18:22:30 -0600

    Tracking works

--- src/navigator.cpp
+++ src/navigator.cpp
@@ -13,6 +13,11 @@
 #include "geometry_msgs/Vector3Stamped.h"
 #include <stdio.h>
 
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+#include <image_transport/image_transport.h>
+#include "OpenCV.cpp"
+
 
 //mavros_msgs::State current_state;
 // void state_cb(const mavros_msgs::State::ConstPtr& msg){
@@ -23,12 +28,25 @@
 //     std::cout << *msg <<std::endl;
 // }
 
+std::pair<int, int> position;
+
+void image_callback(const sensor_msgs::ImageConstPtr& msg) {
+	cout << "yay" << endl;
+	Mat imgOriginal = cv_bridge::toCvCopy(msg, "bgr8")->image;
+	waitKey(30);
+
+    cout << "WHAT" << endl;
+
+    position = detect(imgOriginal);
+}
+
 int main(int argc, char **argv)
 {
    ros::init(argc, argv, "vset");
    ros::NodeHandle n;
 
    ros::Publisher chatter_pub = n.advertise<geometry_msgs::TwistStamped>("/mavros/setpoint_velocity/cmd_vel",100);
+   ros::Subscriber image_sub = n.subscribe("/iris/camera/image_raw", 1, image_callback);
    ros::Rate loop_rate(10);
 
    geometry_msgs::TwistStamped msg;
@@ -36,13 +54,18 @@ int main(int argc, char **argv)
    while(ros::ok()){
        msg.header.stamp = ros::Time::now();
        msg.header.seq=1;
-       msg.twist.linear.x = 1;
-       msg.twist.angular.x = 1;
-       msg.twist.linear.z = 1;
+       // msg.twist.linear.x = 1;
+       // msg.twist.angular.x = 1;
+       // msg.twist.linear.z = 1;
 
        chatter_pub.publish(msg);
        ros::spinOnce();
        loop_rate.sleep();
-   }    
+
+       std::cout << "(" << (position.first - 300) / 200.0 << ", " << -(position.second - 250) / 200.0 << ")" << endl;
+
+       msg.twist.linear.x = -(position.first - 300) / 200.0;
+       msg.twist.linear.y = (position.second - 250) / 200.0;
+   }
    return 0;
 }
